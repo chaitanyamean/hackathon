5929b36ecef660628bbbca5968fcf99fb64209f9
chaitanyamean <40897170+chaitanyamean@users.noreply.github.com>
2023-06-02 09:12:09 +0530

    2 files modified

--- src/pages/CreateIncident.jsx
+++ src/pages/CreateIncident.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const CreateIncident = () => {
-  return <>CreateIncident</>;
+  return (
+    <>
+      {' '}
+      <h1>This is Create Incident Page</h1>
+    </>
+  );
 };
 
 export default CreateIncident;
